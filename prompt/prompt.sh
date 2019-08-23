8c57f645f673c091bb73ac6950673bc1a8b9c2dd
Eric Whitestone <eric.whitestone@gmail.com>
2019-08-23 01:40:27 -0400

    Working plain colors

--- prompt/prompt.sh
+++ prompt/prompt.sh
@@ -19,11 +19,104 @@ CYAN='\033[0;36m'
 LIGHTCYAN='\033[1;36m'
 DEFAULT='\033[0m'
 
-COLORLIST="WHITE LIGHTGRAY GRAY
-BLACK RED LIGHTRED GREEN LIGHTGREEN BROWN YELLOW BLUE LIGHTBLUE PURPLE PINK CYAN LIGHTCYAN DEFAULT" 
+#=========================================================
+# User Configuration
+#=========================================================
+# Colors
+cLINES=$LIGHTGRAY #Lines and Arrow
+cBRACKETS=$LIGHTGRAY # Brackets around each data item
+cERROR=$LIGHTRED # Error block when previous command did not return 0
+cTIME=$LIGHTGRAY # The current time
+cMPX1=$YELLOW # Color for terminal multiplexer threshold 1
+cMPX2=$RED # Color for terminal multiplexer threshold 2
+cBGJ1=$YELLOW # Color for background job threshold 1
+cBGJ2=$RED # Color for background job threshold 2
+cSTJ1=$YELLOW # Color for background job threshold 1
+cSTJ2=$RED # Color for  background job threshold 2
+cSSH=$PINK # Color for brackets if session is an SSH session
+cUSR=$LIGHTBLUE # Color of user
+cUHS=$GRAY # Color of the user and hostname separator, probably '@'
+cHST=$LIGHTGREEN # Color of hostname
+cRWN=$RED # Color of root warning
+cPWD=$BLUE # Color of current directory
+cCMD=$DEFAULT # Color of the command you type
 
-for color in $COLORLIST 
-do
-    echo -e ${!color} $color
-done
+# Enable block
+eNL=1  # Have a newline between previous command output and new prompt
+eERR=1 # Previous command return status tracker
+eSSH=1 # Track if session is SSH
+eUSH=1 # Show user and host
+ePWD=1 # Show current directory
 
+
+function promptcmd()
+{
+        PREVRET=$?
+        #=========================================================
+        #check if user is in ssh session
+        #=========================================================
+        if [[ $SSH_CLIENT ]] || [[ $SSH2_CLIENT ]]; then
+                lSSH_FLAG=1
+        else
+                lSSH_FLAG=0
+        fi
+
+        #=========================================================
+        # Insert a new line to clear space from previous command
+        #=========================================================
+        PS1="\n"
+
+        #=========================================================
+        # Beginning of first line (arrow wrap around and color setup)
+        #=========================================================
+        PS1="${PS1}${cLINES}\342\224\214\342\224\200"
+
+        #=========================================================
+        # First Dynamic Block - Previous Command Error
+        #=========================================================
+        if [ $PREVRET -ne 0 ] ; then
+                PS1="${PS1}${cBRACKETS}[${cERROR}:(${cBRACKETS}]${cLINES}\342\224\200"
+        fi
+
+        #=========================================================
+        # First static block - Current time
+        #=========================================================
+        PS1="${PS1}${cBRACKETS}[${cTIME}\t${cBRACKETS}]${cLINES}\342\224\200"
+
+        #=========================================================
+        # Second Static block - User@host
+        #=========================================================
+        # set color for brackets if user is in ssh session
+        if [ $lSSH_FLAG -eq 1 ] ; then
+                sesClr="$cSSH"
+        else
+                sesClr="$cBRACKETS"
+        fi
+        # don't display user if root
+        if [ $EUID -eq 0 ] ; then
+                PS1="${PS1}${sesClr}[${cRWN}!"
+        else
+                PS1="${PS1}${sesClr}[${cUSR}\u@${cUHS}${UHS}"
+        fi
+        PS1="${PS1}${cHST}\h${sesClr}]${cLINES}\342\224\200"
+
+        #=========================================================
+        # Third Static Block - Current Directory
+        #=========================================================
+        PS1="${PS1}[${cPWD}\w${cBRACKETS}]"
+
+        #=========================================================
+        # Second Line
+        #=========================================================
+        PS1="${PS1}\n${cLINES}\342\224\224\342\224\200\342\224\200> ${cCMD}"
+}
+
+function load_prompt () {
+    # Get PIDs
+
+    export DAY=$(date +%A)
+    PROMPT_COMMAND=promptcmd
+    export PS1 PROMPT_COMMAND
+}
+
+load_prompt
